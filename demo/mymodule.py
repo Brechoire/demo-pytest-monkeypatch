ba6b2b47f717d76ee2d02415ee2762d977c94f6b
Thierry Chappuis <thierry@chappuis.io>
2018-08-19 18:38:58 +0200

    Added comments and docstrings

--- demo/mymodule.py
+++ demo/mymodule.py
@@ -1,9 +1,20 @@
+#-*- coding: utf8 -*-
+
+"""Simple module aiming at demonstrating monkey patching with pytest and
+the monkeypatch fixture.
+
+This module implements a function using the random.choice standard library
+function. This external dependency will be mocked during the unit tests.
+"""
+
 from random import choice
 
 def f(data):
+    """Returns a random item from a list."""
     return choice(data)
 
 def main():
+    """Main entry point."""
     print(f([1, 2, 3, 4, 5]))
 
 if __name__ == "__main__":
--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -2,10 +2,17 @@ from demo.mymodule import f
 
 
 def test_f(monkeypatch, mocker):
+    # Creation of a mock with the pytest-mock plugin to replace the 
+    # random.choice function used in the demo.mymodule module.
     choice_mock = mocker.Mock()
     choice_mock.return_value = "réponse renvoyée par le mock de random.choice"
+
+    # The monkeypatch fixture allows to patch the choice function in 
+    # in demo.mymodule
     monkeypatch.setattr(
         'demo.mymodule.choice', 
         choice_mock
     )
+
+    # f() will now return the same value as choice_mock()
     assert f([1,2,3,4,5]) == choice_mock()
